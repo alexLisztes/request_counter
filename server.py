2fea29e45668071482f746256fccba586900a259
Alex Lisztes <lisztes.alex@gmail.com>
2017-12-13 10:04:34 +0100

    Reworking script to work w/ file instead of global variable.

--- server.py
+++ server.py
@@ -1,30 +1,28 @@
 from flask import Flask, redirect, render_template, request
+import data_handler
 
 app = Flask(__name__)
-
-counts = {
-    'GET': 0,
-    'POST': 0,
-    'DELETE': 0,
-    'PUT': 0,
-}
+filename = "request_counts.txt"
 
 
 @app.route('/')
 def request_counter():
+    counts = data_handler.read_file(filename)
     return render_template("index.html", count=sum(counts.values()))
 
 
 @app.route('/request-counter')
 def increment():
-    global counts
+    counts = data_handler.read_file(filename)
     counts[request.method] += 1
+    data_handler.write_file(filename, counts)
     return redirect('/')
 
 
 @app.route('/statistics')
 def stats():
-    return render_template("statistics.html")
+    counts = data_handler.read_file(filename)
+    return render_template("statistics.html", count=counts)
 
 
 if __name__ == '__main__':
